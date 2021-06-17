730176b0a7855a7111ada05249cf86a4e4e23c76
soft-coded <shrutant123@gmal.com>
2021-06-17 22:24:56 +0530

    DO test

--- src/pages/home/sections/hero/Hero.tsx
+++ src/pages/home/sections/hero/Hero.tsx
@@ -33,8 +33,8 @@ export default function Hero() {
     [imgLeftRef.current, imgRightRef.current].forEach(img =>
       gsap.to(img, {
         duration: 0.7,
-        rotationX: -(frontToBack % 90) * 0.5,
-        rotationY: leftToRight * 0.5,
+        rotationX: -(frontToBack % 90) * 0.35,
+        rotationY: leftToRight * 0.35,
         ease: "power3.out"
       })
     );
